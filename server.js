25f206a6cb689ea594034548b5e95f2f957b25fd
badeniraukrnet <123290328+BadenIra2023@users.noreply.github.com>
2023-09-13 09:04:30 +0300

    update server

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 import app from "./app.js";
 import mongoose from "mongoose";
 
-mongoose.connect(process.env.DB_HOST)
+const { DB_HOST, PORT } = process.env;
+mongoose.connect(DB_HOST)
   .then(() => {
-  app.listen(3000, () => {
-  console.log("Database connection successful")
+    app.listen(PORT, () => {
+  console.log (`Server running. Use our API on port: ${PORT}`);
+  console.log("Database connection successful");
 })
   
   })
