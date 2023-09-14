bf3f77d6de4ad344c3302b419c22b36160983120
badeniraukrnet <123290328+BadenIra2023@users.noreply.github.com>
2023-09-14 16:54:32 +0300

    add auth-router

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ import cors from "cors";
 import dotenv from "dotenv";
 
 import contactsRouter from "./routes/api/contacts-router.js"; 
+import authRouter from "./routes/api/auth-router.js";
 
 dotenv.config();
 const app = express();
@@ -14,7 +15,8 @@ app.use(logger(formatsLogger))
 app.use(cors())
 app.use(express.json())
 
-app.use('/api/contacts', contactsRouter) 
+app.use('/api/contacts', contactsRouter)
+app.use("/api/auth", authRouter)
 
 app.use((req, res) => {
   res.status(404).json({ message: 'Not found' })
